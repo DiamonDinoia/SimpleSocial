4995e47f31db964024893efbf7544e4cae424c2f
Marco <marko-b.94@hotmail.it>
2016-05-24 21:50:58 +0200

    Added isValid to tokens

--- src/marco/rcl/shared/Token.java
+++ src/marco/rcl/shared/Token.java
@@ -1,24 +1,41 @@
 package marco.rcl.shared;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Created by Marco on 24/05/16.
+ * This class models the tokens used for authentication between clients/server, it is automatically generated and not
+ * editable
+ * TODO: would be nice to have a way to check if the Token generated is used by another client
  */
 public class Token {
     private final UUID id;
     private final long timestamp;
+    private final long VALIDITY = TimeUnit.DAYS.toMillis(1);
 
     public Token(long timestamp) {
         this.id = UUID.randomUUID();
         this.timestamp = timestamp;
     }
 
+    /**
+     *
+     * @param obj a token
+     * @return true if the
+     */
     @Override
     public boolean equals(Object obj){
         if ((null == obj) || (obj.getClass() != Token.class))
             return false;
         Token token = (Token) obj;
         return token.id.equals(this.id);
+    }
+
+    /**
+     * This function is used to check if the token is still valid
+     * @retur true if the token is valid, false otherwise
+     */
+    public boolean isValid(){
+       return (System.currentTimeMillis()-timestamp)<VALIDITY;
     }
 }
