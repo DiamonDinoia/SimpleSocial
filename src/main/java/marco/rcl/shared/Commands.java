00e9f4975cc7523bb75976be88887362cd7814e0
Marco <m.barbone@outlook.com>
2018-01-17 01:52:42 +0100

    Final release

---
+++ src/main/java/marco/rcl/shared/Commands.java
@@ -0,0 +1,10 @@
+package marco.rcl.shared;
+
+
+/**
+ * This enum contains all the possible commands shared between client/server.
+ */
+public enum Commands {
+    Register, Login, SearchUser, FriendRequest, FriendConfirm, FriendIgnore, FriendList, Logout, Publish,
+    PendingRequests
+}
